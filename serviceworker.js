47ae4ff474ebed986eecf424db3a6f7f8cd0fd95
rushdanmoengage <rushdan.b@moengage.com>
2025-05-22 09:59:28 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
 	// in your serviceworker.js file
 	// importScripts("<link-that-you-copied>");
 	// for eg, your dashboard host is dashboard-03.moengage.com
-	importScripts("https://cdn.moengage.com/release/dc_3/versions/2/serviceworker_cdn.min.latest.js");
+	importScripts("https://cdn.moengage.com/release/dc_3/versions/2/serviceworker_cdn.min.latest.js?_gl=1*1w1bwem*_gcl_au*Mjk5MjAzNjE2LjE3NDc3NDIxMjI.*_ga*MTczOTA0Nzk3NS4xNzQ3Mzc3NzU1*_ga_SEBHW7YTZ7*czE3NDc4ODc0MjAkbzYkZzEkdDE3NDc4ODgxNDAkajYwJGwwJGgwJGRmZ3J0NkJaem4xUk5xTF9FTDhNU2hTLWdWUXRWQjFvMVJB");
